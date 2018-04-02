965acba102c42d62f1383439fdd419cbdb5a3529
Zluba <742015998@qq.com>
2018-04-02 16:18:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/HelloworldActivity.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloworldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helloworld_layout);
+    }
+}
